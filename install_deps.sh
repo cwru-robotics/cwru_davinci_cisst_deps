d322dec0f5d1a333dcfbabc34e83fe2eea351146
Russell Jackson <rcj33@case.edu>
2017-08-10 09:43:28 -0400

    Completed initial set up of the light cisst repository for the DVRK.  -Added the necessary submodules  -set up travis.yml  -added an install script for libqt5  -Added the JHU CISST dependencies  -Manually added a dependency on tf-conversions which is missing.

---
+++ install_deps.sh
@@ -0,0 +1,10 @@
+
+
+# install qt5
+sudo apt-get install -qq -y qtbase5-dev 
+sudo apt-get install -qq -y libxml2-dev libraw1394-dev libncurses5-dev 
+sudo apt-get install -qq -y qtcreator swig libopenigtlink-dev flite cmake-curses-gui
+sudo apt-get install -qq -y cmake-qt-gui libopencv-dev git subversion gfortran libcppunit-dev fluid
+
+# figure out how to get rid of this:
+sudo apt-get install -qq -y ros-jade-tf-conversions
